0a6dd0e1a8b028417c1a0e3fa543745c5016f6c9
Chris Cantu <christopher.cantu@gmail.com>
2014-01-04 23:45:51 -0600

    Updated project to point to localhost

--- pages/homePage.js
+++ pages/homePage.js
@@ -1,7 +1,7 @@
 var Page = require('astrolabe').Page;
 
 module.exports = Page.create({
-	url: { value: 'http://beta.epikvote.com' },
+	url: { value: 'http://localhost:9000' },
 	title : { value: 'Home Page' },
 	headingTitle: {
 		get: function() { return this.findElement(this.by.css('h3.title')) }
